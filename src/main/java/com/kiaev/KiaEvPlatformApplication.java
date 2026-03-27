e91e41317fc191f1c447bdca6a15b24a9c2e4a03
BHyangie <bhyangie@gmail.com>
2026-03-27 18:47:20 +0900

    feat: 초기 세팅 및 기본 파일 추가

---
+++ src/main/java/com/kiaev/KiaEvPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.kiaev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiaEvPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiaEvPlatformApplication.class, args);
+	}
+
+}
