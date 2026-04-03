d792ef05d732b427891d8cafbcf1c3fd37005f7a
고다영 <dpdvl123@naver.com>
2026-04-03 10:52:24 +0900

    feat:상담내역 관리 기능 구현 완료

---
+++ src/main/java/com/kiaev/admin/consult/AdminConsultRepository.java
@@ -0,0 +1,7 @@
+package com.kiaev.admin.consult;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminConsultRepository extends JpaRepository<AdminConsult, Long> {
+
+}
