412e375eabc68af4ed7937b3f6e2fff47c76964b
syeon <tlfltlfl182@gmail.com>
2026-04-09 12:09:01 +0900

    feat: 상담 신청 페이지 입력 폼 구조 및 UI 개선

--- src/main/java/com/kiaev/client/consult/ConsultRepository.java
+++ src/main/java/com/kiaev/client/consult/ConsultRepository.java
@@ -3,10 +3,13 @@ package com.kiaev.client.consult;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ConsultRepository extends JpaRepository<Consult, Long> {
 
     Optional<Consult> findByConsultNoAndMemberNo(Long consultNo, Long memberNo);
+
+    List<Consult> findByMemberNoOrderByRequestDateDesc(Long memberNo);
 }
