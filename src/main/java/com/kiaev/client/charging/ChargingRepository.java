2e9556c2de7bc089b16e0e5392dd79b0c6e56441
BHyangie <bhyangie@gmail.com>
2026-04-09 15:06:19 +0900

    feat: 지도 통합 검색 기능 및 API 연동 오류 해결

--- src/main/java/com/kiaev/client/charging/ChargingRepository.java
+++ src/main/java/com/kiaev/client/charging/ChargingRepository.java
@@ -1,6 +1,18 @@
 package com.kiaev.client.charging;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChargingRepository extends JpaRepository<Charging, String> {
+	
+	/* // location을 기준으로 충전소 목록을 조회하는 메서드 추가
+    List<Charging> findByLocation(String location); */
+    
+	/*
+ // 수정 후: 해당 단어가 '포함'된 모든 결과를 찾음 (location LIKE "%강남%")
+    List<Charging> findByLocationContaining(String location); */
+    
+ // 이름(statNm)이나 주소(addr)에 검색어가 '포함'된 데이터를 찾습니다.
+    List<Charging> findByStatNmContainingOrAddrContaining(String statNm, String addr);
 }
